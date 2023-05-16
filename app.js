ed941a434d6dd207f89850b70a8d18f151306a84
Nevin06 <thomasnevink@gmail.com>
2023-05-16 20:32:18 +0530

    'Fix'

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-// const express = require('express');
 import express from 'express';
-// const bodyParser = require('body-parser');
 import bodyParser from 'body-parser';
-// const fetch = require('node-fetch');
 import fetch from 'node-fetch';
-// const path = require('path');
 import path from 'path';
-// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app = express();
 
@@ -18,43 +13,25 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/submit-form', async (req, res) => {
-  // const name = req.body.contactName;
-  // const email = req.body.contactEmail;
-  // const message = req.body.contactMessage;
-  // const subject = req.body.contactSubject;
+app.post('/submit-form', (req, res) => {
+  const name = req.body.contactName;
+  const email = req.body.contactEmail;
+  const message = req.body.contactMessage;
+  const subject = req.body.contactSubject;
+  const form = req.body;
 
-  // fetch('https://formspree.io/f/xknaevnn', {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' },
-  //   body: JSON.stringify({ name, email, subject, message }),
-  // })
-  //   .then(() => {
-  //     res.send('Message sent successfully.');
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //     res.send('Error sending message.');
-  //   });
-  try {
-    const form = req.body;
-    const response = await fetch('https://formspree.io/nevinthomas6767@gmail.com', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(form),
+  fetch('https://formspree.io/f/xknaevnn', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, subject, message }),
+  })
+    .then(() => {
+      res.send('Message sent successfully.');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send('Error sending message.');
     });
-
-    if (response.ok) {
-      res.send('Form submitted successfully!');
-    } else {
-      throw new Error('Form submission failed!');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
 });
 
 // Serve static files from the 'Nevin06.github.io' directory
